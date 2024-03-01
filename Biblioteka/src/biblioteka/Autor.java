c7e27710609ffc2b3850f130d91bd455a27dfe2f
vsonja <vukojicicsonja@gmail.com>
2024-03-01 14:54:02 +0100

    Uvedene toString, hashCode i equals metode u klase Autor i Knjiga

--- Biblioteka/src/biblioteka/Autor.java
+++ Biblioteka/src/biblioteka/Autor.java
@@ -1,5 +1,7 @@
 package biblioteka;
 
+import java.util.Objects;
+
 public class Autor {
 
 	private String ime;
@@ -19,6 +21,28 @@ public class Autor {
 
 	public void setPrezime(String prezime) {
 		this.prezime = prezime;
+	}
+
+	@Override
+	public String toString() {
+		return "Autor [ime=" + ime + ", prezime=" + prezime + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(ime, prezime);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Autor other = (Autor) obj;
+		return Objects.equals(ime, other.ime) && Objects.equals(prezime, other.prezime);
 	}
 
 }
--- Biblioteka/src/biblioteka/Knjiga.java
+++ Biblioteka/src/biblioteka/Knjiga.java
@@ -1,6 +1,7 @@
 package biblioteka;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Knjiga {
 
@@ -48,6 +49,29 @@ public class Knjiga {
 
 	public void setIzdanje(int izdanje) {
 		this.izdanje = izdanje;
+	}
+
+	@Override
+	public String toString() {
+		return "Knjiga [naslov=" + naslov + ", isbn=" + isbn + ", autori=" + autori + ", izdavac=" + izdavac
+				+ ", izdanje=" + izdanje + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(isbn);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Knjiga other = (Knjiga) obj;
+		return isbn == other.isbn;
 	}
 
 }
