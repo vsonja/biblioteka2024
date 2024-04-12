bf5f9ad3510fc7284c0a2543b00b54b396031170
vsonja <vukojicicsonja@gmail.com>
2024-04-12 12:52:48 +0200

    Dokumentovana klasa Autor

--- Biblioteka/src/biblioteka/Autor.java
+++ Biblioteka/src/biblioteka/Autor.java
@@ -2,22 +2,65 @@ package biblioteka;
 
 import java.util.Objects;
 
+/**
+ * Predstavlja autora knjige.
+ * 
+ * Autor ima ime i prezime.
+ * 
+ * @author Sonja
+ * 
+ * @version 1.0
+ *
+ */
 public class Autor {
 
+	/**
+	 * Ime autora kao String.
+	 */
 	private String ime;
+	
+	/**
+	 * Prezime autora kao String.
+	 */
 	private String prezime;
 	
+	/**
+	 * Pravi nov objekat klase Autor.
+	 * 
+	 * Ime i prezime ostaju neinicijalizovani.
+	 */
 	public Autor() {}
 
+	/**
+	 * Pravi novog autora i postavlja ime i prezime na unete vrednosti.
+	 * 
+	 * @param ime Ime autora kao String.
+	 * @param prezime Prezime autora kao String.
+	 */
 	public Autor(String ime, String prezime) {
 		this.setIme(ime);
 		this.setPrezime(prezime);
 	}
 
+	/**
+	 * Vraca ime autora.
+	 * 
+	 * @return Trenutno ime autora kao String.
+	 */
 	public String getIme() {
 		return ime;
 	}
 
+	/**
+	 * Postavlja ime autora na unetu vrednost.
+	 * 
+	 * Uneto ime ne sme biti null niti prazan String.
+	 * 
+	 * @param ime Ime autora kao String.
+	 * 
+	 * @throws java.lang.NullPointerException Ako je uneto ime null.
+	 * @throws java.lang.IllegalArgumentException Ako je uneto ime prazan String.
+	 */
 	public void setIme(String ime) {
 		if (ime == null) {
 			throw new NullPointerException("Ime ne sme biti null");
@@ -30,10 +73,25 @@ public class Autor {
 		this.ime = ime;
 	}
 
+	/**
+	 * Vraca prezime autora.
+	 * 
+	 * @return Trenutno prezime autora kao String.
+	 */
 	public String getPrezime() {
 		return prezime;
 	}
 
+	/**
+	 * Postavlja prezime autora na unetu vrednost.
+	 * 
+	 * Uneto prezime ne sme biti null niti prazan String.
+	 * 
+	 * @param prezime Prezime autora kao String.
+	 * 
+	 * @throws java.lang.NullPointerException Ako je uneto prezime null.
+	 * @throws java.lang.IllegalArgumentException Ako je uneto prezime prazan String.
+	 */
 	public void setPrezime(String prezime) {
 		if (prezime == null) {
 			throw new NullPointerException("Prezime ne sme biti null");
@@ -56,6 +114,17 @@ public class Autor {
 		return Objects.hash(ime, prezime);
 	}
 
+	/**
+	 * Poredi dva autora prema imenu i prezimenu.
+	 * 
+	 * @param obj Drugi autor sa kojim se poredi.
+	 * 
+	 * @return
+	 *<ul>
+	 * 		<li><b>true</b> - Ako su oba objekta inicijalizovana, klase su Autor i imaju isto ime i prezime.</li>
+	 * 		<li><b>false</b> - Ako nisu klase Autor, ako je uneti autor null ili ako nije isto ime ili prezime.</li>
+	 *</ul>
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
